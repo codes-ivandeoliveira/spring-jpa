c9182308aef7bfd5e566a5b23848d972843d6ed5
Ivan Oliven <si.ivandeoliveira21@gmail.com>
2022-12-04 15:41:10 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/springjpa/services/UserService.java
+++ src/main/java/com/educandoweb/springjpa/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.springjpa.entities.User;
 import com.educandoweb.springjpa.repositories.UserRepository;
+import com.educandoweb.springjpa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findbyId(Integer id) {
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
@@ -33,6 +34,21 @@ public class UserService {
 	
 	public void delete(Integer id) {
 		repository.deleteById(id);
+		
+				
+	}
+	
+	public User update (Integer id, User obj) {
+		
+		User ententy = repository.getReferenceById(id);
+		updateData(ententy,obj);
+		return repository.save(ententy);
+	}
+
+	private void updateData(User ententy, User obj) {
+		ententy.setName(obj.getName());
+		ententy.setEmail(obj.getEmail());
+		ententy.setPhone(obj.getPhone());
 		
 	}
 }
