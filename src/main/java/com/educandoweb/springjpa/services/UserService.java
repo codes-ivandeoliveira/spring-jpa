bfe5a9f11984cfd127af3a1935506d471b616a59
Ivan Oliven <si.ivandeoliveira21@gmail.com>
2022-12-04 17:39:31 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/springjpa/services/UserService.java
+++ src/main/java/com/educandoweb/springjpa/services/UserService.java
@@ -3,45 +3,53 @@ package com.educandoweb.springjpa.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.loader.plan.exec.process.internal.AbstractRowReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.springjpa.entities.User;
 import com.educandoweb.springjpa.repositories.UserRepository;
+import com.educandoweb.springjpa.services.exceptions.DatabaseException;
 import com.educandoweb.springjpa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List <User> findAll(){
-		
+
+	public List<User> findAll() {
+
 		return repository.findAll();
 	}
-	
+
 	public User findbyId(Integer id) {
-		Optional <User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
-	public User insert (User obj) {
+
+	public User insert(User obj) {
 		return repository.save(obj);
-		
+
 	}
-	
+
 	public void delete(Integer id) {
-		repository.deleteById(id);
-		
-				
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+catch(DataIntegrityViolationException e)
+		{throw new DatabaseException(e.getMessage());}
 	}
-	
-	public User update (Integer id, User obj) {
-		
+
+	public User update(Integer id, User obj) {
+
 		User ententy = repository.getReferenceById(id);
-		updateData(ententy,obj);
+		updateData(ententy, obj);
 		return repository.save(ententy);
 	}
 
@@ -49,6 +57,6 @@ public class UserService {
 		ententy.setName(obj.getName());
 		ententy.setEmail(obj.getEmail());
 		ententy.setPhone(obj.getPhone());
-		
+
 	}
 }
